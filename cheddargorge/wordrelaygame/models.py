1b2ba24a1ca9a8e936700e8069bb51d0be4452d9
Steven Wooding <steve@stevenwooding.com>
2018-04-18 16:07:54 +0100

    feat: Create story and word models

--- cheddargorge/wordrelaygame/models.py
+++ cheddargorge/wordrelaygame/models.py
@@ -1,3 +1,27 @@
+"""Django models for the wordreplaygame app."""
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Story(models.Model):
+    """Model for a story object.
+
+    A story is a collection of at least 64 words.
+    """
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return 'Story #' + self.id
+
+
+class Word(models.Model):
+    """Model for a word object.
+
+    A word object forms part of a story and belongs to a certain user.
+    """
+    content = models.CharField(max_length=64)
+    story = models.ForeignKey(Story, on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.SET_NULL,
+                               blank=True, null=True)
+
+    def __str__(self):
+        return self.content
