965fe9ff12e4c815d6365fdb90df701c120cf76e
Steven Wooding <steve@stevenwooding.com>
2018-04-18 16:52:19 +0100

    feat: Add story and word models to admin

--- cheddargorge/wordrelaygame/admin.py
+++ cheddargorge/wordrelaygame/admin.py
@@ -1,3 +1,14 @@
+"""Control how models are displayed on the Django admin site."""
 from django.contrib import admin
+from .models import Story, Word
 
-# Register your models here.
+class WordInline(admin.TabularInline):
+    """Class to display words that belong to a story inline with that story."""
+    model = Word
+    extra = 1
+
+class StoryAdmin(admin.ModelAdmin):
+    """Defines attributes for displaying a story."""
+    inlines = [WordInline]
+
+admin.site.register(Story, StoryAdmin)
