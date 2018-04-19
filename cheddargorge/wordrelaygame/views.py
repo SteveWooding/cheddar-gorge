3c5f2bb10005edee748601b8a4ea656d1308f890
Steven Wooding <steve@stevenwooding.com>
2018-04-19 16:57:29 +0100

    feat: Add main home view of wordrelayapp

--- cheddargorge/wordrelaygame/views.py
+++ cheddargorge/wordrelaygame/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+"""Views for the wordrelayapp."""
+from django.views.generic import DetailView
 
-# Create your views here.
+from .models import Story
+
+class HomeView(DetailView):
+    """Main view that displays the current story & information about the game.
+
+    Shows the current story and information about the game. If there is a user
+    logged in and it is their turn, it show a form to add a word to the story.
+    """
+    context_object_name = 'latest_story'
+    model = Story
+    template_name = 'wordrelaygame/home.html'
+
+    def get_object(self, queryset=None):
+        try:
+            latest_story = Story.objects.latest('date_created')
+        except Story.DoesNotExist:
+            return None
+        else:
+            return latest_story
