40720cce2260d7b4748bbcf1f298d4b2eb3e613d
Steven Wooding <steve@stevenwooding.com>
2018-04-19 16:39:44 +0100

    fix: Convert story id to str type

--- cheddargorge/wordrelaygame/models.py
+++ cheddargorge/wordrelaygame/models.py
@@ -10,7 +10,7 @@ class Story(models.Model):
     date_created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return 'Story #' + self.id
+        return 'Story #' + str(self.id)
 
 
 class Word(models.Model):
