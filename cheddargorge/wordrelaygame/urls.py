6722804317f450fa531d4c706a6f839d64f3a018
Steven Wooding <steve@stevenwooding.com>
2018-04-20 11:06:14 +0100

    feat: Add URL that handles add a new word

--- cheddargorge/wordrelaygame/urls.py
+++ cheddargorge/wordrelaygame/urls.py
@@ -6,8 +6,8 @@ app_name = 'wordrelaygame'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('addword/', views.AddWordView.as_view(), name='addword'),
     # path('archive/', views.ArchiveView.as_view(), name='archive'),
     # path('story/new/', views.create_story, name='new_story'),
     # path('story/<int:story_id>/', views.StoryView.as_view(), name=''),
-    # path('addword/', views.addword, name='addword'),
 ]
