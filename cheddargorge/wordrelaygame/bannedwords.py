977f4acef01406b26696af9b0ddcfac1efb019c7
Steven Wooding <steve@stevenwooding.com>
2018-04-24 16:40:13 +0100

    feat: Check new word against a list of banned words

---
+++ cheddargorge/wordrelaygame/bannedwords.py
@@ -0,0 +1,7 @@
+"""A list of words that should not be accepted.
+
+Since these will tend to be swear words, they will not be checked
+into Git/GitHub.
+
+"""
+banned_word_list = []
--- cheddargorge/wordrelaygame/forms.py
+++ cheddargorge/wordrelaygame/forms.py
@@ -1,18 +1,30 @@
 """Defines forms for the wordrelaygame app."""
 from django import forms
+from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 
 from .models import Word
+from .bannedwords import banned_word_list
 
 VALID_WORD_REGEX = r'^[A-Za-z\']+\.?,?$'
+
+
+def validate_not_banned_word(value):
+    """Validate that the new word is not or does not contain a banned word."""
+    for word in banned_word_list:
+        if word in value:
+            raise ValidationError('Sorry. That word is or contains a banned ' +
+                                  'word. Please try another word.')
+
 
 class WordForm(forms.ModelForm):
     """Form for added a new word to a story."""
 
     def __init__(self, *args, **kwargs):
         super(WordForm, self).__init__(*args, **kwargs)
-        self.fields['content'].validators.append(
-            RegexValidator(regex=VALID_WORD_REGEX))
+        self.fields['content'].validators.extend((
+            RegexValidator(regex=VALID_WORD_REGEX),
+            validate_not_banned_word))
 
     class Meta:
         model = Word
@@ -21,4 +33,6 @@ class WordForm(forms.ModelForm):
         help_texts = {'content': ('Enter a single word only (apostrophes ' +
                                   'are allowed). You can add a fullstop ' +
                                   'or comma.')}
-        widgets = {'content': forms.TextInput(attrs={'pattern': VALID_WORD_REGEX})}
+        widgets = {
+            'content': forms.TextInput(attrs={'pattern': VALID_WORD_REGEX})
+            }
