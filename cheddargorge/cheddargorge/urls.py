b4acd8db428d6e19bf5606a0eb35e615b23832d6
Steven Wooding <steve@stevenwooding.com>
2018-04-25 10:29:53 +0100

    feat: Wire up the logout URL

--- cheddargorge/cheddargorge/urls.py
+++ cheddargorge/cheddargorge/urls.py
@@ -20,6 +20,8 @@ from django.urls import include, path
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(extra_context={'next': '/'}),
          name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'),
+         name='logout'),
     path('', include('wordrelaygame.urls')),
     path('admin/', admin.site.urls),
 ]
