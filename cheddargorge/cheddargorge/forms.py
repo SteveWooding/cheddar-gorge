fdd466c77b3d177693899d60c9dcd1ecc38357d8
Steven Wooding <steve@stevenwooding.com>
2018-04-25 12:05:36 +0100

    feat: Add customised user creation form

---
+++ cheddargorge/cheddargorge/forms.py
@@ -0,0 +1,13 @@
+"""Forms for user creation."""
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+class UserCreateForm(UserCreationForm):
+    """Customise the user creation form."""
+    class Meta:
+        fields = ('username', 'email', 'password1', 'password2')
+        model = get_user_model()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].label = 'Email Address (optional - for password reset)'
