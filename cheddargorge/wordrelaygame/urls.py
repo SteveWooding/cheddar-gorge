76dc89855ae116e465eaed8b0a3c6025ce907eee
Steven Wooding <steve@stevenwooding.com>
2018-05-17 10:38:21 +0100

    feat: Add route for creating a new story

--- cheddargorge/wordrelaygame/urls.py
+++ cheddargorge/wordrelaygame/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('addword/', views.AddWordView.as_view(), name='addword'),
     # path('archive/', views.ArchiveView.as_view(), name='archive'),
-    # path('story/new/', views.create_story, name='new_story'),
+    path('story/new/', views.AddStoryView.as_view(), name='new_story'),
     # path('story/<int:story_id>/', views.StoryView.as_view(), name=''),
 ]
