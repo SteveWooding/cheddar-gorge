527c1aaa924d05b79fdb3f85864246527d8a9756
Steven Wooding <steve@stevenwooding.com>
2018-05-23 16:44:43 +0100

    feat: Add placeholder for email address on signup form

--- cheddargorge/cheddargorge/forms.py
+++ cheddargorge/cheddargorge/forms.py
@@ -1,4 +1,5 @@
 """Forms for user creation."""
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
@@ -7,7 +8,13 @@ class UserCreateForm(UserCreationForm):
     class Meta:
         fields = ('username', 'email', 'password1', 'password2')
         model = get_user_model()
+        widgets = {
+            'email': forms.TextInput(
+                attrs={'placeholder': 'name@example.com'}
+            ),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['email'].label = 'Email Address (optional - for password reset)'
+        self.fields['email'].label = ('Email Address (optional - for password '
+                                      'reset only)')
