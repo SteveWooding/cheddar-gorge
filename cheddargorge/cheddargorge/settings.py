792cbfc93f27ae1e0245d8530d83ee45b9cf1e22
Steven Wooding <steve@stevenwooding.com>
2018-05-25 11:02:03 +0100

    chore: Sent the email backend to file-based
    This is the setting while developing. Will use a transactional email
    service when deployed on the server.

--- cheddargorge/cheddargorge/settings.py
+++ cheddargorge/cheddargorge/settings.py
@@ -133,3 +133,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
+# Email settings - File-based while developing
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
