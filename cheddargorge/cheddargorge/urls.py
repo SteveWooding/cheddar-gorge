c6a05f655d88482d86ae133cd1a5b0369493c737
Steven Wooding <steve@stevenwooding.com>
2018-05-25 11:11:19 +0100

    style: Put each path argument on separate line to improve readability

--- cheddargorge/cheddargorge/urls.py
+++ cheddargorge/cheddargorge/urls.py
@@ -20,20 +20,47 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(extra_context={'next': '/'}),
-         name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'),
-         name='logout'),
-    path('signup/', views.SignUpView.as_view(), name='signup'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(),
-         name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
-         name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(),
-         name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
-         name='password_reset_complete'),
-    path('', include('wordrelaygame.urls')),
-    path('admin/', admin.site.urls),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(extra_context={'next': '/'}),
+        name='login'
+    ),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(next_page='/'),
+        name='logout'
+    ),
+    path(
+        'signup/',
+        views.SignUpView.as_view(),
+        name='signup'
+    ),
+    path(
+        'password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset'
+    ),
+    path(
+        'password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'
+    ),
+    path(
+        '',
+        include('wordrelaygame.urls')
+    ),
+    path(
+        'admin/',
+        admin.site.urls
+    ),
 ]
