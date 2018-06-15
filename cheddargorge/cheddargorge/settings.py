a53784bc110e36477626e6a0a6bfbb5826646105
Steven Wooding <steve@stevenwooding.com>
2018-06-15 15:28:21 +0100

    test: Turn on template debugging
    This is needed for the Django Coverage plugin to analyse testing
    coverage of templates.

--- cheddargorge/cheddargorge/settings.py
+++ cheddargorge/cheddargorge/settings.py
@@ -65,6 +65,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            # SECURITY WARNING: don't run with debug turned on in production!
+            'debug': True,
         },
     },
 ]
